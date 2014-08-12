23804eba3da9e4a00e50acc7953a76d04a2eb490
sis-nishimur <t-nishimura@sakura-is.co.jp>
2014-08-12 16:29:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoadConsole::Application.config.secret_key_base = '495dbb1cd075ff35a62becc0c9fb27e3bec2741c2d0541cbc310275302d728f531de49d2dc721acc5e448d1a748f4a8f17fc5f9a49a619a5320e75ef6588135d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JoadConsole::Application.config.session_store :cookie_store, key: '_joad-console_session'
