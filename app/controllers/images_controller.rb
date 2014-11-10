414b591bc3a2550d17ce9bd8269022e32c122887
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-11-10 19:07:27 +0900

    イメージの追加を実装

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,18 @@
 class ImagesController < ApplicationController
   def index
+    @new_image = JoadImage.new
     @images = JoadImage.all
+  end
+
+  def create
+    i = JoadImage.new(joad_image_params)
+    i.create
+    redirect_to images_index_path
+  end
+
+  private
+
+  def joad_image_params
+    params.require(:joad_image).permit(:repo_tags => [])
   end
 end
--- app/models/joad_image.rb
+++ app/models/joad_image.rb
@@ -7,21 +7,37 @@ class JoadImage
 
   class << self
     def all
-      Docker::Image.all.map {|i| new(i) }
+      Docker::Image.all.map {|i| convert(i) }
+    end
+
+    def convert(docker_image)
+      params = {
+        'id' => docker_image.id,
+        'size' => docker_image.info['VirtualSize'],
+        'created' => Time.at(docker_image.info['Created']),
+        'repo_tags' => docker_image.info['RepoTags'].map do |rt|
+          repo, tag = rt.split(':')
+          { repo: repo, tag: tag }
+        end
+      }
+      new(params)
     end
   end
 
-  def initialize(docker_image)
-    @id = docker_image.id
-    @size = docker_image.info['VirtualSize']
-    @created = Time.at(docker_image.info['Created'])
-    @repo_tags = docker_image.info['RepoTags'].map do |rt|
-      repo, tag = rt.split(':')
-      { repo: repo, tag: tag }
-    end
+  def initialize(params = {})
+    @id = params['id']
+    @size = params['size']
+    @created = params['created']
+    @repo_tags = params['repo_tags']
   end
 
   def short_id
     @id[0...12]
+  end
+
+  def create
+    @repo_tags.each do |rt|
+      Docker::Image.create(fromImage: rt)
+    end if @repo_tags
   end
 end
