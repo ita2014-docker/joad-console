5287f255003917b47e5fad36c180733a21a192ca
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-11-16 22:41:54 +0900

    Add route for start container

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ JoadConsole::Application.routes.draw do
     get 'images' => :index, as: :images_index
     get 'images/:id' => :show, as: :images_show
     post 'images/create' => :create, as: :images_create
+    post 'images/start_container' => :start_container, as: :start_container
   end
 end
