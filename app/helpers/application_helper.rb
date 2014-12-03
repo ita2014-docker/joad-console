52ac717033d4b5d490ef85103e39b6b5668e5b35
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-12-03 14:36:50 +0000

    Change is_runnning? to glyphicons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def ok?(bool)
+    result = bool ? '<span class="text-success glyphicon glyphicon-ok" aria-hidden="true"></span>'
+      : '<span class="text-danger glyphicon glyphicon-remove" aria-hidden="true"></span>'
+    result.html_safe
+  end
 end
