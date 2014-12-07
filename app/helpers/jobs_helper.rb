77d115b178f4f301a467f335fc392b6c335c5089
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-12-07 06:26:33 +0000

    job status icons

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,22 @@
 module JobsHelper
+  def job_status_icon(status)
+    case status
+    when 'success'
+      '<span class="text-success glyphicon glyphicon-ok" aria-hidden="true"></span>'
+    when 'failure'
+      '<span class="text-danger glyphicon glyphicon-remove" aria-hidden="true"></span>'
+    when 'unstable'
+      '<span class="text-warning glyphicon glyphicon-warning-sign" aria-hidden="true"></span>'
+    when 'running'
+      '<span class="text-info glyphicon glyphicon-flash" aria-hidden="true"></span>'
+    when 'not_run'
+      '<span class="text-muted glyphicon glyphicon-question-sign" aria-hidden="true"></span>'
+    when 'aborted'
+      '<span class="text-danger glyphicon glyphicon-minus" aria-hidden="true"></span>'
+    when 'invalid'
+      '<span class="text-muted glyphicon glyphicon-ban-circle" aria-hidden="true"></span>'
+    else
+      '<span class="text-muted glyphicon glyphicon-question-sign" aria-hidden="true"></span>'
+    end.html_safe
+  end
 end
