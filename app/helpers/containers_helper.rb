5bd76e49392ff914febd6b91dccaca8cc3a6419b
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-12-07 06:44:34 +0000

    Update running container status icons

--- app/helpers/containers_helper.rb
+++ app/helpers/containers_helper.rb
@@ -1,2 +1,10 @@
 module ContainersHelper
+  def running_status_icon(is_running)
+    case is_running
+    when true
+      '<span class="text-success glyphicon glyphicon-play" aria-hidden="true"></span>'
+    else
+      '<span class="text-danger glyphicon glyphicon-stop" aria-hidden="true"></span>'
+    end.html_safe
+  end
 end
